7cdee4136612949c3acc12d1ecc4dc04bfb8a940
fantasista21jp <info@fantasista21jp.com>
2010-12-13 11:35:40 +0900

    first commit

---
+++ js/jquery.hinttextbox.js
@@ -0,0 +1,50 @@
+/*
+ * jQuery hinttextbox (jQuery Plugin)
+ *
+ * Copyright (c) 2010 Tom Shimada
+ *
+ * Depends Script:
+ *	jquery.js (1.3.2)
+ */
+
+(function($) {
+  $.fn.hinttextbox = function(configs){
+    var defaults = {
+          text: null
+        };
+    if (!configs) return;
+    configs = $.extend(defaults, configs);
+    if (typeof(configs.text) !== 'string') return;
+    var $textbox = this;
+    if (typeof($textbox) !== 'object' || $textbox.length !== 1) return;
+
+    set();
+
+    $textbox.focus(function(){
+      unset();
+    }).blur(function(){
+      set();
+    });
+
+    var $form = $textbox.parents('form:first');
+    if (typeof($form) === 'object' || $form.length === 1) {
+      $form.submit(function(){
+        unset();
+      });
+    }
+
+    function set() {
+      var val = $textbox.val();
+      if (val && val != configs.text) return;
+      $textbox.val(configs.text).css('color', '#999999');
+    }
+
+    function unset() {
+      var val = $textbox.val();
+      if (val && val != configs.text) return;
+      $textbox.val('').css('color', '');
+    }
+
+    return this;
+  };
+})(jQuery);
